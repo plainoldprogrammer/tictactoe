d5e6b6ab7c6d60ddca3100884025980c7bd5e2d7
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-10-02 12:38:12 -0500

    marking the winner (only for the first column case)

--- main.cpp
+++ main.cpp
@@ -422,4 +422,18 @@ void markTheWinner(SDL_Renderer *m_window_renderer, std::string location)
 		SDL_RenderFillRect(m_window_renderer, &lineHorizontal);
 		SDL_RenderPresent(m_window_renderer);
 	}
+	else if (location.compare("First Column") == 0)
+	{
+		LOG(INFO) << "Marking the first column...";
+
+		SDL_Rect lineHorizontal;
+		lineHorizontal.x = 100;
+		lineHorizontal.y = 20;
+		lineHorizontal.w = 20;
+		lineHorizontal.h = 440;
+
+		SDL_SetRenderDrawColor(m_window_renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(m_window_renderer, &lineHorizontal);
+		SDL_RenderPresent(m_window_renderer);
+	}
 }
